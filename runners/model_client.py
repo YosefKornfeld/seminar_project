78f3e2cba2b02e5836e618563b2daa20c2268bd0
yosefk7 <yosefk7@gmail.com>
2026-06-05 09:59:34 +0300

    hanoi completed, 'n' changed for a few puzzles

--- runners/model_client.py
+++ runners/model_client.py
@@ -43,8 +43,8 @@ EXPERIMENT_CONFIG = {
     ],
     "puzzles": {
         "hanoi": [3, 4, 5, 6, 7],
-        "river_crossing": [2, 3, 4],
-        "blocks_world": [2, 4, 6], # Even N required for blocks world logic
+        "river_crossing": [2, 4],
+        "blocks_world": [2, 6, 10], # Even N required for blocks world logic
         "checker_jumping": [1, 2, 3]
     },
     "samples_per_config": 10,
